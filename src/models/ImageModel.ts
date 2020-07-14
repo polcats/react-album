9fbcbbd42cc8958e9b52e8b1ac958e8e92d83cd0
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-14 15:44:52 +0800

    Removed setters on image model.

--- src/models/ImageModel.ts
+++ src/models/ImageModel.ts
@@ -2,10 +2,10 @@ import { model, Model, prop } from 'mobx-keystone';
 
 @model('albumApp/ImageModel')
 export class ImageModel extends Model({
-  id: prop(String, { setterAction: true }),
-  author: prop(String, { setterAction: true }),
-  width: prop(Number, { setterAction: true }),
-  height: prop(Number, { setterAction: true }),
-  url: prop(String, { setterAction: true }),
-  download_url: prop(String, { setterAction: true }),
+  id: prop(String),
+  author: prop(String),
+  width: prop(Number),
+  height: prop(Number),
+  url: prop(String),
+  download_url: prop(String),
 }) {}
